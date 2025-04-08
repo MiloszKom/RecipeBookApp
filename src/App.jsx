fb666ab225f7f9bb5a921089e4a241568a875b4a
MiloszKom <milosz.kom@onet.pl>
2025-04-08 19:24:47 +0200

    chore: configure Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 class="text-3xl font-bold underline">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
